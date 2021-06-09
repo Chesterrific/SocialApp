b241347239c7339515dd801626ebf9f2d370b4d0
Chester Guinto <cguinto@uscorp.centrillionbio.com>
2021-06-08 18:00:34 -0700

    Added express for streamlined api functions. dbschema added for scream reference.

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "scream body",
+      createdAt: "2021-06-09T00:57:06.953Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,95 @@
 const functions = require("firebase-functions");
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const express = require("express");
+const app = express();
 
-// How to fetch collections
-exports.getScreams = functions.https.onRequest((request, response) => {
+app.get("/screams", (req, res) => {
   admin
     .firestore()
-    .collection('screams')
+    .collection("screams")
+    .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
       let screams = [];
       data.forEach((doc) => {
-        screams.push(doc.data());
+        screams.push({
+          screamID: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
-      return response.json(screams);
+      return res.json(screams);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-    })
+    });
 });
+
+app.post("/scream", (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+// How to fetch collections w/out Express.
+// exports.getScreams = functions.https.onRequest((request, response) => {
+//   admin
+//     .firestore()
+//     .collection("screams")
+//     .get()
+//     .then((data) => {
+//       let screams = [];
+//       data.forEach((doc) => {
+//         screams.push(doc.data());
+//       });
+//       return response.json(screams);
+//     })
+//     .catch((err) => {
+//       console.error(err);
+//     });
+// });
+
+// Create new item in collection, in this case a new scream w/out Express.
+// exports.createScream = functions.https.onRequest((req, res) => {
+//   if (req.method != "POST") {
+//     return res.status(400).json({ error: "method not allowed" });
+//   }
+//   const newScream = {
+//     body: req.body.body,
+//     userHandle: req.body.userHandle,
+//     createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+//   };
+
+//   admin
+//     .firestore()
+//     .collection("screams")
+//     .add(newScream)
+//     .then((doc) => {
+//       res.json({ message: `document ${doc.id} created successfully` });
+//     })
+//     .catch((err) => {
+//       res.status(500).json({ error: "something went wrong" });
+//       console.error(err);
+//     });
+// });
+
+// https://baseurl.com/api/ formatting
+exports.api = functions.https.onRequest(app);
