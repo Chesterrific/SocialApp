4d3a1eeef574da5badf65885bf03bad754e4ea20
Chester Guinto <cguinto@uscorp.centrillionbio.com>
2021-06-23 15:44:38 -0700

    Added user authentication to app.

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,18 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
+const app = require("express")();
 
 admin.initializeApp();
 
-const express = require("express");
-const app = express();
+const firebase = require("firebase");
+const { config } = require("./config.js");
+firebase.initializeApp(config);
 
+const db = admin.firestore();
+
+// Express data base get.
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
+  db.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -29,11 +32,53 @@ app.get("/screams", (req, res) => {
     });
 });
 
+// Express database post.
 app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
     createdAt: new Date().toISOString(),
+  };
+
+  db.collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+});
+
+/*
+How to fetch collections w/out Express.
+exports.getScreams = functions.https.onRequest((request, response) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return response.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+Create new item in collection, in this case a new scream w/out Express.
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method != "POST") {
+    return res.status(400).json({ error: "method not allowed" });
+  }
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
   };
 
   admin
@@ -48,48 +93,54 @@ app.post("/scream", (req, res) => {
       console.error(err);
     });
 });
+*/
 
-// How to fetch collections w/out Express.
-// exports.getScreams = functions.https.onRequest((request, response) => {
-//   admin
-//     .firestore()
-//     .collection("screams")
-//     .get()
-//     .then((data) => {
-//       let screams = [];
-//       data.forEach((doc) => {
-//         screams.push(doc.data());
-//       });
-//       return response.json(screams);
-//     })
-//     .catch((err) => {
-//       console.error(err);
-//     });
-// });
+// Signup Route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+  //todo: validate data.
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res.status(400).json({ handle: "this handle is taken." });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
+      } else {
+        return res.status(500).json({ error: err.code });
+      }
+    });
+});
 
-// Create new item in collection, in this case a new scream w/out Express.
-// exports.createScream = functions.https.onRequest((req, res) => {
-//   if (req.method != "POST") {
-//     return res.status(400).json({ error: "method not allowed" });
-//   }
-//   const newScream = {
-//     body: req.body.body,
-//     userHandle: req.body.userHandle,
-//     createdAt: admin.firestore.Timestamp.fromDate(new Date()),
-//   };
-
-//   admin
-//     .firestore()
-//     .collection("screams")
-//     .add(newScream)
-//     .then((doc) => {
-//       res.json({ message: `document ${doc.id} created successfully` });
-//     })
-//     .catch((err) => {
-//       res.status(500).json({ error: "something went wrong" });
-//       console.error(err);
-//     });
-// });
-
-// https://baseurl.com/api/ formatting
 exports.api = functions.https.onRequest(app);
